a2253ebf1df585014e414ea3a18feeb45eb6c3ad
Raghav Sharma <raghavs@adobe.com>
2023-07-27 16:12:02 +0530

    A few improvements to the final webpage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { docx2dast, dast2mdast } from '@adobe/helix-docx2md';
+import { docx2md } from '@adobe/helix-docx2md';
 import getHast from '@adobe/helix-html-pipeline/src/utils/mdast-to-hast.js';
 import splitSections from '@adobe/helix-html-pipeline/src/steps/split-sections.js';
 import getMetadata from '@adobe/helix-html-pipeline/src/steps/get-metadata.js';
@@ -12,10 +12,14 @@ import createPictures from '@adobe/helix-html-pipeline/src/steps/create-pictures
 import addHeadingIds from '@adobe/helix-html-pipeline/src/steps/add-heading-ids.js';
 import render from '@adobe/helix-html-pipeline/src/steps/render.js';
 import removeHlxProps from '@adobe/helix-html-pipeline/src/steps/removeHlxProps.js';
+import extractMetadata from '@adobe/helix-html-pipeline/src/steps/extract-metadata.js';
+import stringify from '@adobe/helix-html-pipeline/src/steps/stringify-response.js';
+import parseMarkdown from '@adobe/helix-html-pipeline/src/steps/parse-markdown.js';
 
 import { readFileSync, writeFileSync, watchFile } from 'node:fs';
 import { toHtml } from 'hast-util-to-html';
-import GithubSlugger from 'github-slugger'
+import GithubSlugger from 'github-slugger';
+import { execSync } from 'child_process';
 
 function setDummyMetadata(state) {
   state.content = {
@@ -35,24 +39,37 @@ function setDummyMetadata(state) {
   }
 }
 
-async function docx2mdast(doc, opts) {
-  const dast = await docx2dast(doc, {});
-  const mdast = await dast2mdast(dast, opts); 
+async function docx2html(doc, opts) {
+  console.info('Converting Docx to html');
+  const md = await docx2md(doc, {});
   const state = { 
     content: {
-      mdast,
+      data: md,
       slugger: new GithubSlugger(),
     },
     info: {
-      selector: 'notplain'
+      selector: 'notplain',
+      path: '',
+    },
+    metadata: {
+      getModifiers: (...args) => ({}),
+    },
+    mappedMetadata: {
+      getModifiers: (...args) => ({}),
+    },
+    log: {
+      warn: (...args) => console.warn(args)
+    },
+    config: {
+      host: 'localhost'
     },
     helixConfig: {
       head: {
         data: {
           html:`
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <script src="../helpx-internal/scripts/fallback.js" nomodule></script>
-            <script src="../helpx-internal/scripts/scripts.js" type="module"></script>
+            <script src="/scripts/fallback.js" nomodule></script>
+            <script src="/scripts/scripts.js" type="module"></script>
             <style>body { display: none; }</style>
             <link rel="icon" href="data:,">
           ` 
@@ -60,8 +77,9 @@ async function docx2mdast(doc, opts) {
       }
     }
   };
-  const req = {};
+  const req = { url: {pathname: ''}};
   const res = { document: {} };
+  await parseMarkdown(state);
   await splitSections(state);
   await getMetadata(state);
   await unwrapSoleImages(state);
@@ -71,25 +89,34 @@ async function docx2mdast(doc, opts) {
   await fixSections(state);
   await createPageBlocks(state);
   await createPictures(state);
-  // skipping extractMetadata
   setDummyMetadata(state);
+  await extractMetadata(state, req);
   await addHeadingIds(state)
   await render(state, req, res);
   await removeHlxProps(state, req, res);
-  return res.document;
+  await stringify(state, req, res);
+  console.info('done');
+  return res.body;
 }
 
+const [docx] = process.argv.slice(2);
+
 const doTheThing = () => {
-  const file = readFileSync('../cci-cct-etf-guidelines.docx');
-  docx2mdast(file, {}).then(r => {
-    const final = toHtml(r, { upperDoctype: true})
-    writeFileSync('../out.html', final);
+  const file = readFileSync(docx);
+  docx2html(file, {}).then(r => {
+    writeFileSync('helpx-internal/out.html', r);
   });
 }
 
+execSync('rm -rf helpx-internal');
+execSync('git clone https://github.com/adobecom/helpx-internal.git', {
+  stdio: [0, 1, 2], // we need this so node will print the command output
+  cwd: '.', // path to where you want to save the file
+})
+
 doTheThing();
 
-watchFile('../cci-cct-etf-guidelines.docx',() => {
+watchFile(docx ,() => {
   console.log('Change Detected');
   doTheThing();
 });
